a828076838f9a4f2d5791d7ba6ab81a40068a951
Abhishek Gupta (knoxxs) <knoxxs@email.com>
2014-02-10 19:51:09 +0530

    Add play again feature

--- game.py
+++ game.py
@@ -14,16 +14,22 @@ def main():
 	4) Continue until you guess it right!
 	And you win... :-) Kudos!
 	"""
+    
+    play_again = 'y'
+    while play_again == 'y':
+        num = random.random()*100 // 1
+        guess = ""
 
-    num = random.random()*100 // 1
-    guess = ""
-
-    while guess != num:
-        guess = int(raw_input("Take a guess: "))
-        if guess < num:
-            print "Guess higher next time\n"
-        elif guess > num:
-            print "Guess lower next time\n"
-    print "Voila! You got it!\nNow you know the deep secrets of life, universe and everything...\n"
-
+        while guess != num:
+            guess = int(raw_input("Take a guess: "))
+            if guess < num:
+                print "Guess higher next time\n"
+            elif guess > num:
+                print "Guess lower next time\n"
+        print "Voila! You got it!\nNow you know the deep secrets of life, universe and everything...\n"
+    
+        play_again = raw_input("Wanna play again (y/n): ")
+        if play_again == 'n':
+            print "Bye..."
+            break
 main()
